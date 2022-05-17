9a2172e2de052142bdfa7e3e9fdc39dfc0e80df4
Samuel Owusu <mailto:owususamuel12@gmail.com>
2022-05-17 21:19:52 +0000

    designed the settings UI screen using xml

---
+++ app/src/main/java/com/sammy/android_ui_settings/SettingsFragment.kt
@@ -0,0 +1,18 @@
+package com.sammy.android_ui_settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import androidx.preference.PreferenceFragmentCompat
+
+
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
+    }
+
+
+}
