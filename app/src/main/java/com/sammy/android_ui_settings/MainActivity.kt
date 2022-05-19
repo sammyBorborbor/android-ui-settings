0b2b4bddde9f662483518abce362724167734050
Samuel Owusu <mailto:owususamuel12@gmail.com>
2022-05-19 21:35:40 +0000

    created a settings ui with kotlin code

--- app/src/main/java/com/sammy/android_ui_settings/MainActivity.kt
+++ app/src/main/java/com/sammy/android_ui_settings/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.sammy.android_ui_settings
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.preference.PreferenceManager
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),  SharedPreferences.OnSharedPreferenceChangeListener{
 
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -27,9 +31,44 @@ class MainActivity : AppCompatActivity() {
 
         // Link ActionBar with NavController
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+        val autoReplyTime = sharedPreferences.getString(getString(R.string.key_auto_reply_time), "")
+        Log.d("MainActivity", "onCreatePreferences: $autoReplyTime")
+
+        val publicInfo: Set<String>? = sharedPreferences.getStringSet(getString(R.string.key_public_info), null)
+        Log.i("TEST", "onCreate: $publicInfo")
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        if(key == getString(R.string.key_status)) {
+            val newStatus = sharedPreferences?.getString(key, "")
+            Toast.makeText(this, "New Status: $newStatus", Toast.LENGTH_SHORT).show()
+        }
+
+        if(key == getString(R.string.key_auto_reply)) {
+            val autoReply = sharedPreferences?.getBoolean(key, false)
+            if(autoReply!!) {
+                Toast.makeText(this, "Auto Reply: ON", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Auto Reply: OFF", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this)
     }
 }
--- app/src/main/java/com/sammy/android_ui_settings/SettingsFragment.kt
+++ app/src/main/java/com/sammy/android_ui_settings/SettingsFragment.kt
@@ -1,17 +1,57 @@
 package com.sammy.android_ui_settings
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.NavHostFragment
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
+
+        val accSettingsPref = findPreference<Preference>(getString(R.string.key_account_settings))
+
+        accSettingsPref?.setOnPreferenceClickListener {
+            val navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_frag) as NavHostFragment
+            val navController = navHostFragment.navController
+            val action = SettingsFragmentDirections.actionSettingsToAccSettings()
+            navController.navigate(action)
+
+            true
+        }
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
+
+        val autoReplyTime = sharedPreferences.getString(getString(R.string.key_auto_reply_time), "")
+        Log.d("TEST", "onCreatePreferences: $autoReplyTime")
+
+        val autoDownload = sharedPreferences.getBoolean(getString(R.string.key_auto_download), false)
+        Log.d("TEST", "onCreatePreferences: $autoDownload")
+
+
+        val statusPref = findPreference<EditTextPreference>(getString(R.string.key_status))
+        statusPref?.setOnPreferenceChangeListener { preference, newValue ->
+            Log.i("TEST", "onPreferenceChange: $newValue")
+
+            val newStatus = newValue as String
+
+            if(newStatus.contains("bad")) {
+                Toast.makeText(requireContext(), "Inappropriate status. Please maintain community guidelines", Toast.LENGTH_SHORT).show()
+                false
+            } else {
+                true
+            }
+        }
+
     }
 
 
